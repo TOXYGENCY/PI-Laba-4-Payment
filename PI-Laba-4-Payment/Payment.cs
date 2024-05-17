ecf43ed29178e2d463bba1d6a84125c37e89b2cb
TOXYGENCY <mailforsomethingelse@gmail.com>
2024-05-17 23:02:31 +0300

    Add project files.

---
+++ PI-Laba-4-Payment/Payment.cs
@@ -0,0 +1,22 @@
+﻿namespace PI_Laba_4_Payment
+{
+    public class Payment
+    {
+        public string ContractorName { get; protected set; }
+        public int PaymentAmount { get; protected set; }
+
+        public Payment(string? name, int amount)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Имя должно быть указано!");
+            }
+            else
+            {
+                ContractorName = name;
+                PaymentAmount = amount;
+            }
+        }
+
+    }
+}
---
+++ PI-Laba-4-Payment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+
+namespace PI_Laba_4_Payment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Payment> PaymentList = new List<Payment>
+            {
+                new Payment("Agent1", 100000),
+                new Payment("Agent1", 210002),
+                new Payment("Agent1", 99870),
+                new Payment("Agent2", 1500),
+                new Payment("Agent4", 9455),
+            };
+            Console.WriteLine("Введите имя контрагента для вычисления общей суммы платежей: ");
+            var selector = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(selector))
+            {
+                throw new ArgumentException("Имя должно быть указано!");
+            }
+            else
+            {
+                var LINQ = from payment in PaymentList
+                           where payment.ContractorName == selector
+                           select payment.PaymentAmount;
+                var TotalAgentSum = LINQ.Sum();
+
+                Console.WriteLine(TotalAgentSum);
+            }
+        }
+    }
+}
